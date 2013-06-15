a26b79244b131b51fdc739eb2d21646690d95b13
Antonio <antalby@hotmail.com>
2013-06-15 22:54:36 +0200

    commit

--- find2.rb
+++ find2.rb
@@ -1,13 +1,20 @@
 def trova(file = ARGV.first, word = ARGV[1], new_file = ARGV.last)
 file_in_string = File.read(file)
-r_in_array = file_in_string.scan(/^\d+\. .*\b#{Regexp.escape(word)}.*\.\#$/m)
+r_in_array = file_in_string.scan(/^\d+\. [^\b#]*/)
 File.open(new_file, "w") do |file|
 r_in_array.each do |result|
-file.puts result
+file.puts result if result.include?(word)
 end
 end
 end
 
+def length_array(file = ARGV.first)
+file_in_string = File.read(file)
+r_in_array = file_in_string.scan(/^\d+\. [^\b#]*/).length
+end
+
+puts length_array
 trova
+ #must be 500 quizzes
 
 #formula da applicare:
