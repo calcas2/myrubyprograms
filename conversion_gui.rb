e44ebff3dd158cef804ab74ccc18b06729eabf3d
Antonio <antalby@hotmail.com>
2013-07-05 00:39:15 +0200

    improve gui

--- conversion_gui.rb
+++ conversion_gui.rb
@@ -18,41 +18,61 @@ class Conversion
 
 
   def convert
-   value, units = @text.value.split
-   case units
+   case $value_radio_button
    when /cf/i
-   $val.value = from_celsius_to_fahrenheit(value)
+   @val.value = from_celsius_to_fahrenheit(@text.value)
    when /fc/i
-   $val.value = from_fahrenheit_to_celsius(value)
+   @val.value = from_fahrenheit_to_celsius(@text.value)
    else
-   $val.value = "unknown conversion"
+   @val.value = "unknown conversion"
    end
    end
 
   def initialize
     ph = { 'padx' => 10, 'pady' => 10 }     # common options
     p = proc {convert}
-    $val = TkVariable.new
+    $value_radio_button = TkVariable.new
+    @val = TkVariable.new
     @text = TkVariable.new
     root = TkRoot.new { title "Conversion" }
     top = TkFrame.new(root)
     TkLabel.new(top) {
-      text   "type the value you want to convert followed by the space(' ') and 'cf' if you want to convert from Celsius to Fahrenheit or by 'fc' if you want to convert from Fahrenheit to Celsius"
-     pack(ph) }
+      text   "type the value you want to convert"
+      textvariable
+       borderwidth 5
+       font TkFont.new('times 20 bold')
+       foreground  "red"
+       relief      "groove"
+       pack(ph) }
     @entry = TkEntry.new(top, 'textvariable' => @text)
     @entry.pack(ph)
+    TkRadioButton.new(top) {
+  	text 'from Celsius to Fahrenheit'
+  	variable $value_radio_button
+  	value 'cf'
+ 	anchor 'w'
+  	pack(ph)
+	}
+	TkRadioButton.new(top) {
+ 	 text 'from Fahrenheit to Celsius'
+  	variable $value_radio_button
+  	value 'fc'
+  	anchor 'w'
+  	pack(ph)
+	}
+ 
     TkButton.new(top) {text 'Convert it!';  command p; pack ph}
     TkButton.new(top) {text 'Exit'; command {proc exit}; pack ph}
     top.pack('fill'=>'both', 'side' =>'top')
-    $lbl = TkLabel.new(root) do
+    @lbl = TkLabel.new(root) do
   textvariable
   borderwidth 5
   font TkFont.new('times 20 bold')
   foreground  "red"
   relief      "groove"
-  pack("side" => "right",  "padx"=> "50", "pady"=> "50")
+  pack(ph)
 end
-$lbl['textvariable'] = $val
+@lbl['textvariable'] = @val
   end
 end
 
